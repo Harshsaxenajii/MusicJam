45405809cd6dc5fc69a84a9f9afe48ee571ad651
harsh saxena <harshsaxenajii@gmail.com>
2023-04-08 17:08:57 +0530

    fix

--- src/components/Player.jsx
+++ src/components/Player.jsx
@@ -1,25 +1,10 @@
-import React, { useEffect, useRef, useState } from "react";
+import React, { useEffect, useState } from "react";
 
 function Player(props) {
-  const audioRef = useRef();
-  const [song] = useState(props.musicLink);
-  console.log(song);
-
-  useEffect(() => {
-    updateSong();
-  }, [song]);
-
-  const updateSong = (song) => {
-    if (audioRef.current) {
-      audioRef.current.pause();
-      audioRef.current.load();
-      audioRef.current.play();
-    }
-  };
   return (
     <div>
-      <audio controls autoPlay="true" ref={audioRef}>
-        <source src={song} />
+      <audio controls autoPlay="true">
+        <source src={props.musicLink} />
       </audio>
     </div>
   );
