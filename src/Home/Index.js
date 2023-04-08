63b7ae3adda297b40a89418caad7abaf8e16822e
Duevolt1 <hackx1456@gmail.com>
2023-04-08 17:09:34 +0530

    fix

--- src/Home/Index.js
+++ src/Home/Index.js
@@ -1,26 +1,10 @@
-import React, { useEffect, useState } from "react";
+import React from "react";
 import { BrowserRouter as Router, Routes, Route } from "react-router-dom";
 import Login from "../auth/Login";
 import Home from "../components/Home";
-import { setClientToken } from "../spotify";
 
 function Index() {
-  const [token, setToken] = useState("");
-  useEffect(() => {
-    const token = window.localStorage.getItem("token");
-    const hash = window.location.hash;
 
-    window.location.hash = "";
-    if (!token && hash) {
-      const _token = hash.split("&")[0].split("=")[1];
-      window.localStorage.setItem("token", _token);
-      setToken(_token);
-      setClientToken(_token);
-    } else {
-      setToken(token);
-      setClientToken(token);
-    }
-  }, []);
   return (
     <Router>
       <Routes>
